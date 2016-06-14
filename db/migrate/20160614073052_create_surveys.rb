7cb8d8b46328f7f6ec34db924eb04a8ee01a41c4
Su Su Aung <susu.aung@innoboost.co.kr>
2016-06-14 16:32:19 +0900

    initial commit

---
+++ db/migrate/20160614073052_create_surveys.rb
@@ -0,0 +1,13 @@
+class CreateSurveys < ActiveRecord::Migration
+  def change
+    create_table :surveys do |t|
+      t.text :deviceId
+      t.string :faculty
+      t.int :q1Ans
+      t.text :q2Ans
+      t.string :q3Ans
+
+      t.timestamps null: false
+    end
+  end
+end
